91db7145f03e931e91818880b4c56a3eaf782393
Francisco Garache <franciscogarach3@gmail.com>
2022-05-26 21:36:09 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,16 @@
+print("Imprima los números del 1 a 10 al revés utilizando el ciclo for \n")
+var n = 10
+for i in   1...10 {
+    print(i, "- Numero", n)
+    n = n-1
+}
+
+
+print("\n Crear un bucle que cuente todos los números pares hasta el 100 \n")
+
+var n2 = 0 
+while (n2 != 10){
+    
+    n2 = n2+1
+    print("numero = ",n2)
+}
